a64f2572778758fc065cf3f37a5ae0a9d49b1fad
narasena <cursedsena@gmail.com>
2025-07-25 16:45:47 +0700

    chore: initialize api with java spring boot

---
+++ api/src/main/java/com/narasena/xmart_grocery_api/XmartGroceryApiApplication.java
@@ -0,0 +1,13 @@
+package com.narasena.xmart_grocery_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmartGroceryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmartGroceryApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/narasena/xmart_grocery_api/XmartGroceryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narasena.xmart_grocery_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmartGroceryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
